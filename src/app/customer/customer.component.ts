50c2889eaeb8a9cfc2d147bd2f41cf107bbc7c8f
calvinvette <calvin.vette@nextgeneducation.com>
2017-08-30 17:08:04 +0000

    Lab 7 Solution

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -11,7 +11,10 @@ export class CustomerComponent implements OnInit, AfterViewInit {
   private static _customers: any = {
     "1234": new Customer(1234, "Harry", "James", "Potter", "harry.potter@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1980, 6, 31)),
     "1235": new Customer(1235, "Ron", "Bilius", "Weasley", "ronald.weasley@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1980, 2, 1)),
-    "1236": new Customer(1236, "Hermione", "Jean", "Granger", "hermione.granger@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1979, 8, 19))
+    "1236": new Customer(1236, "Hermione", "Jean", "Granger", "hermione.granger@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1979, 8, 19)),
+    "1237": new Customer(1237, "Neville", "", "Longbottom", "hermione.granger@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1979, 8, 19)),
+    "1238": new Customer(1238, "Dean", "", "Thomas", "hermione.granger@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1979, 8, 19)),
+    "1239": new Customer(1239, "Seamus", "", "Finnegan", "hermione.granger@hogwarts.ac.uk", "+44 0206 491-1331", new Date(1979, 8, 19))
   };
   private _customer: Customer;
   @Input('customerId')
@@ -49,6 +52,16 @@ export class CustomerComponent implements OnInit, AfterViewInit {
 
   onClick($event) {
     console.log("Clicked on age of " + this.customer.firstName);
+  }
+
+  onHover($event) {
+    console.log("Hovering over: ");
+    console.log(this.customer);
+    console.log($event);
+  }
+
+  ageInYears() : number {
+    return Math.floor(this.customer.age);
   }
 
 
